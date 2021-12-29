c429949e13caa36e6d666253b4deee0b91cbb704
bobbywip <84804011+bobbywip@users.noreply.github.com>
2021-12-28 17:41:04 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -235,7 +235,7 @@ function App() {
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
         image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
       >
-        <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
+        <StyledLogo alt={"logo"} src={"/config/images/logo2.png"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
           <s.Container flex={1} jc={"center"} ai={"center"}>
