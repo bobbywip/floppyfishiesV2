ee39098f72f88cad479af02f2843e68c49bbe648
bobbywip <84804011+bobbywip@users.noreply.github.com>
2021-12-29 18:54:40 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,6 +28,68 @@ export const StyledButton = styled.button`
   }
 `;
 
+
+export const StyledButton2 = styled.button`
+  		align-items: center;
+		  appearance: none;
+		  background-image: radial-gradient(100% 100% at 100% 0, #0099ff 0, #66ff66 100%);
+		  border: 0;
+		  left: 30%;
+		  border-radius: 6px;
+		  box-shadow: rgba(45, 35, 66, .4) 0 2px 4px,rgba(45, 35, 66, .3) 0 7px 13px -3px,rgba(58, 65, 111, .5) 0 -3px 0 inset;
+		  box-sizing: border-box;
+		  color: #fff;
+		  cursor: pointer;
+		  display: inline-flex;
+		  font-family: "JetBrains Mono",monospace;
+		  height: 48px;
+		  justify-content: center;
+		  line-height: 1;
+		  list-style: none;
+		  overflow: hidden;
+		  padding-left: 16px;
+		  padding-right: 16px;
+		  position: relative;
+		  text-align: left;
+		  text-decoration: none;
+		  transition: box-shadow .15s,transform .15s;
+		  user-select: none;
+		  -webkit-user-select: none;
+		  touch-action: manipulation;
+		  white-space: nowrap;
+		  will-change: box-shadow,transform;
+		  font-size: 18px;
+      
+      :after {
+		  background-image: radial-gradient(100% 100% at 100% 0, #0099ff 0, #66ff66 100%);
+		  border-radius: 8px;
+		  opacity: 0;
+		  content: "";
+		  display: inline;
+		  height: 48px;
+		  left: 0;
+		  width: 100%;
+		  position: absolute;
+		  top: -2px;
+		  transform: translate(8px, 8px);
+		  transition: transform .2s ease-out;
+		  z-index: -1;
+		}
+    :focus {
+		  box-shadow: #99ff99 0 0 0 1.5px inset, rgba(45, 35, 66, .4) 0 2px 4px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #99ff99 0 -3px 0 inset;
+		}
+
+		:hover {
+		  box-shadow: rgba(45, 35, 66, .4) 0 4px 8px, rgba(45, 35, 66, .3) 0 7px 13px -3px, #99ff99 0 -3px 0 inset;
+		  transform: translateY(-2px);
+		}
+
+		:active {
+		  box-shadow: #99ff99 0 3px 7px inset;
+		  transform: translateY(2px);
+		}
+`;
+
 export const StyledRoundButton = styled.button`
   padding: 10px;
   border-radius: 100%;
@@ -229,6 +291,16 @@ function App() {
 
   return (
     <s.Screen>
+                      <StyledButton2
+                        onClick={(e) => {
+                          e.preventDefault();
+                          console.log("Clicked Button");
+                        }}
+                      >
+                        +
+                      </StyledButton2>
+    
+    
       <s.Container
         flex={1}
         ai={"center"}
